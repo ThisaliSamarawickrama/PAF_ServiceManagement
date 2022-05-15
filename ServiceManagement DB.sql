3d8aad17097910b06c86f4727dd055ddadbf9232
ThisaliSamarawickrama <89655089+ThisaliSamarawickrama@users.noreply.github.com>
2022-05-15 23:05:17 +0530

    ServiceManagement DB

---
+++ ServiceManagement DB.sql
@@ -0,0 +1,5 @@
+notificationID,topic,description,date
+2100,Management,"Bill Price",2020/05/25
+2101,Management,"Bill price",2020/05/10
+2102,Bill,"Bill price = 2000",2020/06/20
+2103,"Bill Details","Bill Price = 5000",2021/02/25
